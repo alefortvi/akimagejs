9078fcac2c4f90f36a8b15f4570ad381b08b55a1
alefortvi <alefortvi@yahoo.com.ar>
2013-11-13 02:40:33 -0300

    commit

---
+++ demos/Modules/js/AkLUT.js
@@ -0,0 +1,43 @@
+
+
+Array.max = function( array ){
+    return Math.max.apply( Math, array );
+};
+
+Array.min = function( array ){
+    return Math.min.apply( Math, array );
+};
+
+
+/********************* Convolucion **********************/
+
+
+
+//Imagen original de muestra 
+
+
+
+var canvas1 = document.getElementById('canvas1');
+var canvas2 = document.getElementById('canvas2');
+
+var img = new Image();
+img.src = './../../images/lenna256.jpg';
+	
+
+
+var Ak4 = AkLoadImage(img,1);
+var R1 = AkCreateROI(70,70,100,100);
+AkSetImageROI(Ak4,R1);
+
+L = [];
+for(var t=0;t<256;t++){
+
+    L[t] = 255-t;
+}
+
+var Ak5 = AkLUT(Ak4,L,false);
+
+AkLoadOnCanvas(Ak5,canvas1);
+
+var Ak6 = AkCrop(Ak4);
+AkLoadOnCanvas(Ak6,canvas2);
