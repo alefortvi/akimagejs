3701780ef1db25d76482262ad93f036dc9fb39d9
alefortvi <alefortvi@yahoo.com.ar>
2014-09-11 21:39:24 -0300

    Deleted some stuff

--- libs/build/builder.bat
+++
@@ -1,5 +0,0 @@
-@echo on
-DEL /F /S /Q /A "compilated\akimage.js"
-type 00_namespace.js 01_constants.js 02_objects.js 03_constructors.js 04_ROI.js 05_Histogram.js 06_modules.js 07_tools.js 08_color.js 09_GUI.js >> compilated/akimage.js
-
-@echo builded
